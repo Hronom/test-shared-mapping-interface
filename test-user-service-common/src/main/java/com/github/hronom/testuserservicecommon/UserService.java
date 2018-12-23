e32c2dd5d5013b4f0f91dd188270ac274439e852
Eugene <hronom@gmail.com>
2018-12-23 18:45:53 +0200

    Added root RequestMapping 2

--- test-user-service-common/src/main/java/com/github/hronom/testuserservicecommon/UserService.java
+++ test-user-service-common/src/main/java/com/github/hronom/testuserservicecommon/UserService.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@RequestMapping(value ="/users", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+@RequestMapping(value ="/users", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 public interface UserService {
     @RequestMapping(method = RequestMethod.GET, value ="/{id}")
     User getUser(@PathVariable("id") String id);
